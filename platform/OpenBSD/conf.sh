5f2c7a3235b072b694fc7763599a40be80456629
Tim Kuijsten <info+git@netsend.nl>
2025-07-23 14:02:18 +0200

    OpenBSD pf: remove ifdef HAS_PFVAR_H
    Depends on pf(4) which was introduced in OpenBSD 3.0 in 2001.

--- platform/OpenBSD/conf.sh
+++ platform/OpenBSD/conf.sh
@@ -6,8 +6,3 @@ case `grep -csq "ds_rxfer" /usr/include/sys/disk.h` in
 1)	echo "#define HAS_IO2	1" ;;
 0)	echo "#undef HAS_IO2" ;;
 esac;
-if [ -f /usr/include/net/pfvar.h ]; then
-    echo "#define HAS_PFVAR_H	1"
-else
-    echo "#undef HAS_PFVAR_H"
-fi
